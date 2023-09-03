5482f919e6bec7f716e722c8a2c0a86bb1af0cf1
ckm-c <chuakiminclement@gmail.com>
2023-09-03 15:41:50 +0800

    category

--- client/src/App.js
+++ client/src/App.js
@@ -40,8 +40,20 @@ const App= () => {
 
 
   //sortedTask checks if tasks variable is not null then sorts the task based on the task date property. sort function takes a comparison function that compares the dates and returns a negative, zero, or positive value to determine the order
-  const sortedTasks = tasks?.sort((a,b)=> new Date(b.date) - new Date(a.date))
+  // const sortedTasks = tasks?.sort((a,b)=> new Date(b.date) - new Date(a.date))
+
+  // current state for category to be all
+  const [selectedCategory, setSelectedCategory] = useState('all'); //default
   
+  // return the filtered category
+  const filteredTasks = tasks?.filter(task => {
+    if (selectedCategory === 'all'){
+      return true;
+    }
+    return task.category ===selectedCategory;
+  })
+  .sort((a,b)=> new Date(b.date) - new Date(a.date))
+
   // Return what will be rendered on the screen when this component is used
   return (
     <div>
@@ -52,9 +64,44 @@ const App= () => {
         {authToken &&  
         <>
         <ListHeader listName={`📝 ${user} ToDo List`} getData = {getData} /> 
+        
         <p className = "user-email">Welcome back {user}</p>
+              
+        <div className="category-filter">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="all"
+              checked={selectedCategory === 'all'}
+              onChange={() => setSelectedCategory('all')}
+            />
+            All
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="work"
+              checked={selectedCategory === 'work'}
+              onChange={() => setSelectedCategory('work')}
+            />
+            Work
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              checked={selectedCategory === 'personal'}
+              onChange={() => setSelectedCategory('personal')}
+            />
+            Personal
+          </label>
+      </div>
+
           {/* Maps through sortedTasks if exists, render ListItem component for each task, passing key prop with task._id and task prop */}
-          { sortedTasks?.map((task)=> <ListItem key={task._id} task={task} getData={getData} />)}
+          { filteredTasks?.map((task)=> <ListItem key={task._id} task={task} getData={getData} />)}
           </>}
     
       </div>
